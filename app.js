e31fc97a554b41d1c5e6170b388d067754b93526
Enrique W <kique10000@gmail.com>
2013-09-16 15:17:20 -0400

    deployment

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.configure(function () {
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.logger());
   app.use(express.bodyParser());
-  app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
